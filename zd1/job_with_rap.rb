545e391b2c8c6c275cac2e62219a08a85e5cf793
MoRain666 <morainrallez@gmail.com>
2017-10-21 15:44:05 +0300

    под курсы v.2

--- zd1/job_with_rap.rb
+++ zd1/job_with_rap.rb
@@ -18,9 +18,79 @@ class RapProcessing
       puts "Подсчёт невозможен для #{@name_of_battle_local}"
       puts "Отсутствует текст на странице!"
       puts '='*20
-    else
+    elsif @rap.length == 6
+      @count_of_first_raper = character_count(@rap[0]) + character_count(@rap[2]) + character_count(@rap[4])
+      @count_of_second_raper = character_count(@rap[1]) + character_count(@rap[3]) + character_count(@rap[5])
+      if @count_of_first_raper > @count_of_second_raper
+        puts "Название рэп-баттла : #{@name_of_battle_local}"
+        puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
+        puts 'Победил первый рэпер'
+        puts '='*20
+      else
+        puts "Название рэп-баттла : #{@name_of_battle_local}"
+        puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
+        puts 'Победил второй рэпер'
+        puts '='*20
+      end
+    elsif @rap.length == 7
       @count_of_first_raper = character_count(@rap[1]) + character_count(@rap[3]) + character_count(@rap[5])
       @count_of_second_raper = character_count(@rap[2]) + character_count(@rap[4]) + character_count(@rap[6])
+      if @count_of_first_raper > @count_of_second_raper
+        puts "Название рэп-баттла : #{@name_of_battle_local}"
+        puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
+        puts 'Победил первый рэпер'
+        puts '='*20
+      else
+        puts "Название рэп-баттла : #{@name_of_battle_local}"
+        puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
+        puts 'Победил второй рэпер'
+        puts '='*20
+      end
+    elsif @rap.length == 8
+      @count_of_first_raper = character_count(@rap[0]) + character_count(@rap[2]) + character_count(@rap[4]) + character_count(@rap[6])
+      @count_of_second_raper = character_count(@rap[1]) + character_count(@rap[3]) + character_count(@rap[5]) + character_count(@rap[7])
+      if @count_of_first_raper > @count_of_second_raper
+        puts "Название рэп-баттла : #{@name_of_battle_local}"
+        puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
+        puts 'Победил первый рэпер'
+        puts '='*20
+      else
+        puts "Название рэп-баттла : #{@name_of_battle_local}"
+        puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
+        puts 'Победил второй рэпер'
+        puts '='*20
+      end  
+    elsif @rap.length == 9
+      @count_of_first_raper = character_count(@rap[1]) + character_count(@rap[3]) + character_count(@rap[5]) + character_count(@rap[7])
+      @count_of_second_raper = character_count(@rap[2]) + character_count(@rap[4]) + character_count(@rap[6]) + character_count(@rap[8])
+      if @count_of_first_raper > @count_of_second_raper
+        puts "Название рэп-баттла : #{@name_of_battle_local}"
+        puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
+        puts 'Победил первый рэпер'
+        puts '='*20
+      else
+        puts "Название рэп-баттла : #{@name_of_battle_local}"
+        puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
+        puts 'Победил второй рэпер'
+        puts '='*20
+      end
+    elsif @rap.length == 10
+      @count_of_first_raper = character_count(@rap[0]) + character_count(@rap[2]) + character_count(@rap[4]) + character_count(@rap[6]) + character_count(@rap[8])
+      @count_of_second_raper = character_count(@rap[1]) + character_count(@rap[3]) + character_count(@rap[5]) + character_count(@rap[7]) + character_count(@rap[9])
+      if @count_of_first_raper > @count_of_second_raper
+        puts "Название рэп-баттла : #{@name_of_battle_local}"
+        puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
+        puts 'Победил первый рэпер'
+        puts '='*20
+      else
+        puts "Название рэп-баттла : #{@name_of_battle_local}"
+        puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
+        puts 'Победил второй рэпер'
+        puts '='*20
+      end  
+    elsif @rap.length == 11
+      @count_of_first_raper = character_count(@rap[1]) + character_count(@rap[3]) + character_count(@rap[5]) + character_count(@rap[7]) + character_count(@rap[9])
+      @count_of_second_raper = character_count(@rap[2]) + character_count(@rap[4]) + character_count(@rap[6]) + character_count(@rap[8]) + character_count(@rap[10])
       if @count_of_first_raper > @count_of_second_raper
         puts "Название рэп-баттла : #{@name_of_battle_local}"
         puts "У первого рэпера #{@count_of_first_raper} букв,а у второго #{@count_of_second_raper} букв "
